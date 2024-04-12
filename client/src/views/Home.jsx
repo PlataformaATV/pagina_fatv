942b07836d46f764cb7a0127cfc9a816a0b576ba
Deiiivy <deivypr28@gmail.com>
2024-04-12 18:34:45 -0500

    adding structure

---
+++ client/src/views/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
+
+function Home() {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home

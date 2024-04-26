592d0e1d049d30548819abb920b65523f40e7a25
samuelzapatam22 <samuelzapatam1@gmail.com>
2024-04-26 17:01:36 -0500

    add aboutUs component to Home

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../components/Header/Header'
 import OurAllies from '../components/our_allies/Our_allies'
 import Footer from '../components/Footer/Footer'
+import AboutUs from '../components/aboutUs/AboutUs'
 
 
 function Home() {
   return (
     <div>
       <Header />
+      <AboutUs />
       <OurAllies />
     </div>
   )

3da114159f041fe726179276a05153a929655a9a
Deiiivy <deivypr28@gmail.com>
2024-04-26 15:14:43 -0500

    delete btn stripe

--- client/src/components/btns-wompi-and-donation/Btns.jsx
+++ client/src/components/btns-wompi-and-donation/Btns.jsx
@@ -8,10 +8,7 @@ function Btns() {
                 <p className='text-xl font-bold text-white'>Donar</p>
                 <box-icon name='heart' type='solid' color='#f3f3f3'></box-icon>
             </div>
-            <div className='flex gap-2 items-center rounded-3xl bg-btn-back px-3 h-8' style={{ marginLeft: '-1px', borderLeft: '1px solid #f3f3f3' }}>
-                <p className='text-2xl font-bold text-white'>Stripe</p>
-                <box-icon type='logo' name='stripe'></box-icon>
-            </div>
+           
         </Link>
     );
 }

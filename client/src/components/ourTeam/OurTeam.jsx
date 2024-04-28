4023fe436ff144a86d2f7118301541fa499ad45f
samuelzapatam22 <samuelzapatam1@gmail.com>
2024-04-28 16:34:24 -0500

    add animations to Team Cards

--- client/src/components/ourTeam/OurTeam.jsx
+++ client/src/components/ourTeam/OurTeam.jsx
@@ -1,6 +1,17 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "./OurTeam.css";
 
 function OurTeam() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  };
+
   return (
     <div className="OurTeam-block">
       <div className="OurTeam-block-title">
@@ -8,138 +19,73 @@ function OurTeam() {
       </div>
       <div className="OurTeam">
         <div className="cards">
-          {/* Inicio card 1*/}
-          <div className="card">
-            <div className="image">
-              <img src="/img_OurTeam/CarlosVasquez.jpeg" alt="Carlos Vásquez" />
-            </div>
-            <div className="content">
-              <div className="content-info">
-                <h5 className="content-info-name">Carlos vásquez restrepo</h5>
-                <p className="content-info-role">Presidente</p>
+          <Slider {...settings}>
+            {cards.map((card, index) => (
+              <div key={index} className="card">
+                <div className="image">
+                  <img src={card.image} alt={card.name} />
+                </div>
+                <div className="content">
+                  <div className="content-info">
+                    <h5 className="content-info-name">{card.name}</h5>
+                    <p className="content-info-role">{card.role}</p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          {/* Fin card 1*/}
-          {/* Inicio card 2*/}
-          <div className="card">
-            <div className="image">
-              <img src="/img_OurTeam/AstridFranco.jpg" alt="Astrid Franco" />
-            </div>
-            <div className="content">
-              <div className="content-info">
-                <h5 className="content-info-name">astrid franco</h5>
-                <p className="content-info-role">Coordinadora Área Social</p>
-              </div>
-            </div>
-          </div>
-          {/* Fin card 2*/}
-          {/* Inicio card 3*/}
-          <div className="card">
-            <div className="image">
-              <img src="/img_OurTeam/LuisSanchez.jpeg" alt="Luis Sanchez" />
-            </div>
-            <div className="content">
-              <div className="content-info">
-                <h5 className="content-info-name">luis fernando sánchez</h5>
-                <p className="content-info-role">Director</p>
-              </div>
-            </div>
-          </div>
-          {/* Fin card 3*/}
-          {/* Inicio card 4*/}
-          <div className="card">
-            <div className="image">
-              <img src="/img_OurTeam/KarenGonzales.jpg" alt="Karen Gonzáles" />
-            </div>
-            <div className="content">
-              <div className="content-info">
-                <h5 className="content-info-name">karen gonzález</h5>
-                <p className="content-info-role">
-                  Coordinadora Área Evaluación y Datos
-                </p>
-              </div>
-            </div>
-          </div>
-          {/* Fin card 4*/}
-          {/* Inicio card 5*/}
-          <div className="card">
-            <div className="image">
-              <img
-                src="/img_OurTeam/VictorValencia.jpeg"
-                alt="Victor Valencia"
-              />
-            </div>
-            <div className="content">
-              <div className="content-info">
-                <h5 className="content-info-name">Víctor Manuel Valencia</h5>
-                <p className="content-info-role">Subdirector</p>
-              </div>
-            </div>
-          </div>
-          {/* Fin card 5*/}
-          {/* Inicio card 6*/}
-          <div className="card">
-            <div className="image">
-              <img src="/img_OurTeam/JazminAlzate.jpeg" alt="Jazmin Alzate" />
-            </div>
-            <div className="content">
-              <div className="content-info">
-                <h5 className="content-info-name">jazmín alzate</h5>
-                <p className="content-info-role">
-                  Coordinadora Alianzas y Cooperación
-                </p>
-              </div>
-            </div>
-          </div>
-          {/* Fin card 6*/}
-          {/* Inicio card 7*/}
-          <div className="card">
-            <div className="image">
-              <img src="/img_OurTeam/DavidBotero.jpeg" alt="David Botero" />
-            </div>
-            <div className="content">
-              <div className="content-info">
-                <h5 className="content-info-name">david santiago botero</h5>
-                <p className="content-info-role">Coordinador Área Legal</p>
-              </div>
-            </div>
-          </div>
-          {/* Fin card 7*/}
-          {/* Inicio card 8*/}
-          <div className="card">
-            <div className="image">
-              <img src="/img_OurTeam/DanielaCorrea.jpeg" alt="Daniela Correa" />
-            </div>
-            <div className="content">
-              <div className="content-info">
-                <h5 className="content-info-name">daniela correa</h5>
-                <p className="content-info-role">
-                  Coordinadora Área Comunicaciones
-                </p>
-              </div>
-            </div>
-          </div>
-          {/* Fin card 8*/}
-          {/* Inicio card 9*/}
-          <div className="card">
-            <div className="image">
-              <img src="/img_OurTeam/LuisGonzales.jpg" alt="Luis Gonzalez" />
-            </div>
-            <div className="content">
-              <div className="content-info">
-                <h5 className="content-info-name">luis fernando gonzález</h5>
-                <p className="content-info-role">
-                  Coordinador Área Administrativa y Financiera
-                </p>
-              </div>
-            </div>
-          </div>
-          {/* Fin card 9*/}
+            ))}
+          </Slider>
         </div>
       </div>
     </div>
   );
 }
+
+const cards = [
+  {
+    name: "Carlos vásquez restrepo",
+    role: "Presidente",
+    image: "/img_OurTeam/CarlosVasquez.jpeg",
+  },
+  {
+    name: "astrid franco",
+    role: "Coordinadora Área Social",
+    image: "/img_OurTeam/AstridFranco.jpg",
+  },
+  {
+    name: "luis fernando sánchez",
+    role: "Director",
+    image: "/img_OurTeam/LuisSanchez.jpeg",
+  },
+  {
+    name: "karen gonzález",
+    role: "Coordinadora Área Evaluación y Datos",
+    image: "/img_OurTeam/KarenGonzales.jpg",
+  },
+  {
+    name: "Víctor Manuel Valencia",
+    role: "Subdirector",
+    image: "/img_OurTeam/VictorValencia.jpeg",
+  },
+  {
+    name: "jazmín alzate",
+    role: "Coordinadora Alianzas y Cooperación",
+    image: "/img_OurTeam/JazminAlzate.jpeg",
+  },
+  {
+    name: "david santiago botero",
+    role: "Coordinador Área Legal",
+    image: "/img_OurTeam/DavidBotero.jpeg",
+  },
+  {
+    name: "daniela correa",
+    role: "Coordinadora Área Comunicaciones",
+    image: "/img_OurTeam/DanielaCorrea.jpeg",
+  },
+  {
+    name: "luis fernando gonzález",
+    role: "Coordinador Área Administrativa y Financiera",
+    image: "/img_OurTeam/LuisGonzales.jpg",
+  },
+];
 
 export default OurTeam;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,7 +8,14 @@ export default {
     extend: {
       colors: {
         'blue-links': '#222D56',
-        'btn-back': '#7c78b3'
+        'btn-back': '#7c78b3',
+        'title' : '#33526d',
+        'ourteam' : '#ffba08',
+        'content' : '#232e55',
+        
+      },
+      fontFamily: {
+        'oswald': ['Oswald'],
       },
     },
   },

5bec82c4188bb1f27f48c497a8b524b1137ef6d8
Monalisa330 <laurameliza26@gmail.com>
2024-04-29 16:35:03 -0500

    start componet services

---
+++ client/src/components/Services_ATV/Services_ATV.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Services_ATV.css'; // Importa el archivo CSS
+import 'boxicons'
+
+const ServicesATV = () => {
+  return (
+    <div className='servicesATV'>
+
+      <div>
+
+        <div className='flex'> 
+        <box-icon type='solid' name='folder-open'></box-icon>
+        <h3>
+        PRESTAMOS LOS SIGUIENTES SERVICIOS
+        </h3>
+        </div>
+
+      </div>
+
+      <div>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default ServicesATV;
--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -5,6 +5,7 @@ import Footer from '../components/Footer/Footer'
 import AboutUs from '../components/aboutUs/AboutUs'
 import OurTeam from '../components/ourTeam/OurTeam'
 import ContextData from '../components/ContextData/ContextData'
+import ServicesATV from '../components/Services_ATV/Services_ATV'
 
 
 function Home() {
@@ -12,6 +13,7 @@ function Home() {
     <div>
       <Header />
       <AboutUs />
+      <ServicesATV />
       <OurTeam />
       <ContextData />
       <OurAllies />

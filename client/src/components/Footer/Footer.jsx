f12d915ce9d39c1d29afe593e2051c46e852549d
brallam delgado <brallamdelgado351@gmail.com>
2024-04-29 18:13:18 -0500

    Add component banner and footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import "./Footer.css"; // Asegúrate de que la ruta del archivo CSS sea correcta
+import logo from "../../../public/logo.png"
 
 function Footer() {
   return (
-    <footer>
-      <div>
-        <img src="" alt="" />
+    <footer >
+      <div className='logo'>
+        <img src={logo} alt="logo-footer" />
       </div>
-      <div className="footer-content">
+      <div className="footer-visitanos">
         <h2>Visítanos</h2>
         <p>Calle 79 sur # 50-165 - oficinas 301 y 401 La Estrella - Antioquia-colombia</p>
       </div>
-      <div>
+      <div className='footer-contacto'>
         <h2>Llámanos o escríbenos</h2>
-        <p>contactenos@fundacionantivirinevvrnlrwfnvwvtrv..com</p>
+        <p>contactenos@fundacionantivirinevvrnlrwfnvwvtrv.com</p>
         <p>WhatsApp/+573226367781</p>
       </div>
-      <div>
+      <div className='footer-redes'>
         <h2>Nuestras redes sociales</h2>
+        <box-icon name='facebook-circle' type='logo' color='#ffffff' ></box-icon>
+        <box-icon name='instagram-alt' type='logo' color='#ffffff' ></box-icon>
+        <box-icon name='youtube' type='logo' color='#ffffff' ></box-icon>
+        <box-icon name='tiktok' type='logo' color='#ffffff' ></box-icon>
+        <box-icon name='linkedin' type='logo' color='#ffffff' ></box-icon>
       </div>
-      <div style={{ order: 5 }}> {/* Cambia a un objeto de estilo */}
+      <div className='footer-copy'> {/* Cambia a un objeto de estilo */}
         <p>Antivirus para la desercion @ 2024 all rights reserved    política de protección de datos</p>
       </div>
-      <div style={{ order: 6 }}> {/* Cambia a un objeto de estilo */}
+      <div className='footer-redirecion'> {/* Cambia a un objeto de estilo */}
         flecha
       </div>
     </footer>

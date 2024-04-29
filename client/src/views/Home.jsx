dd15ac5ba2813988e4c492a04b403eda1a626a7b
Deiiivy <deivypr28@gmail.com>
2024-04-29 18:32:14 -0500

    update README

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -5,16 +5,19 @@ import Footer from '../components/Footer/Footer'
 import AboutUs from '../components/aboutUs/AboutUs'
 import OurTeam from '../components/ourTeam/OurTeam'
 import ContextData from '../components/ContextData/ContextData'
+import Banner from '../components/Banner/Banner'
 
 
 function Home() {
   return (
     <div>
       <Header />
+      <Banner />
       <AboutUs />
       <OurTeam />
       <ContextData />
       <OurAllies />
+      <Footer />
     </div>
   )
 }

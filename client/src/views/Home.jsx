b6d802b58f52288e0ded152e18cd17a9ec149603
Deiiivy <deivypr28@gmail.com>
2024-04-28 22:31:45 -0500

    resolve conflicts

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -2,25 +2,17 @@ import React from 'react'
 import Header from '../components/Header/Header'
 import OurAllies from '../components/our_allies/Our_allies'
 import Footer from '../components/Footer/Footer'
-<<<<<<< HEAD
 import AboutUs from '../components/aboutUs/AboutUs'
 import OurTeam from '../components/ourTeam/OurTeam'
-=======
-import ContextData from '../components/ContextData/ContextData'
->>>>>>> feature/ContextData
 
 
 function Home() {
   return (
     <div>
       <Header />
-<<<<<<< HEAD
       <AboutUs />
       <OurTeam />
       <OurAllies />
-=======
-      <ContextData />
->>>>>>> feature/ContextData
     </div>
   )
 }

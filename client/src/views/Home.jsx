9eae8b24957e04ae818ca355b65a3ef71a32a3ed
Monalisa330 <laurameliza26@gmail.com>
2024-04-28 21:40:42 -0500

    Mensaje descriptivo del commit

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
+import ContextData from '../components/ContextData/ContextData'
 
 
 function Home() {
   return (
     <div>
       <Header />
+      <ContextData />
     </div>
   )
 }

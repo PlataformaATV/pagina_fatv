b85e4c3e4c07cf55fe7e5847dc356960e1444c68
brallam delgado <brallamdelgado351@gmail.com>
2024-05-08 14:55:42 -0500

    responsive Banner

--- client/src/components/Banner/Banner.jsx
+++ client/src/components/Banner/Banner.jsx
@@ -8,6 +8,7 @@ import banner4 from "../../assets/Banner/banner4.jpeg";
 import banner5 from "../../assets/Banner/banner5.jpeg";
 import banner6 from "../../assets/Banner/banner6.jpeg";
 import banner7 from "../../assets/Banner/banner7.jpeg";
+import banner8 from "../../assets/Banner/banner8.jpeg"
 import "./Banner.css";
 
 
@@ -67,22 +68,29 @@ const Banner = () => {
             <p>A TRAVÉS DEL APOYO SOCIOPEDAGÓGICO, LAS COMUNICACIONES Y LA ANALÍTICA DE DATOS</p>
           </div>
         </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100 h-100" src={banner8} alt="Slide 7" />
+          <div className="carousel-text" >
+            <h1>FOMENTAMOS LA PERMANENCIA ESTUDIANTIL</h1>
+            <p>A TRAVÉS DEL APOYO SOCIOPEDAGÓGICO, LAS COMUNICACIONES Y LA ANALÍTICA DE DATOS</p>
+          </div>
+        </Carousel.Item>
       </Carousel >
       <div className="icon-container">
         <a href="https://www.facebook.com/people/Fundaci%C3%B3n-Antivirus-para-la-Deserci%C3%B3n/100089714876149/?mibextid=LQQJ4d" target="_blank" rel="noopener noreferrer">
-          <box-icon type='logo' name='facebook-circle'></box-icon>
+          <box-icon className="box-icon" name='facebook-circle' type='logo' color='#ffffff' ></box-icon>
         </a>
         <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-          <box-icon name='instagram-alt' type='logo'></box-icon>
+          <box-icon className="box-icon" name='instagram-alt' type='logo' color='#ffffff' ></box-icon>
         </a>
         <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
-          <box-icon name='youtube' type='logo'></box-icon>
+          <box-icon className="box-icon" name='youtube' type='logo' color='#ffffff' ></box-icon>
         </a>
         <a href="https://www.tiktok.com/" target="_blank" rel="noopener noreferrer">
-          <box-icon name='tiktok' type='logo' ></box-icon>
+          <box-icon className="box-icon" name='tiktok' type='logo' color='#ffffff' ></box-icon>
         </a>
         <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
-          <box-icon name='linkedin-square' type='logo' ></box-icon>
+         <box-icon className="box-icon" name='linkedin-square' type='logo' color='#ffffff' ></box-icon>
         </a>
       </div>
     </div>

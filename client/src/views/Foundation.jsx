f85c36676c47c713c99121f8f83d2a8f91f65965
Monalisa330 <laurameliza26@gmail.com>
2024-05-28 19:46:31 -0500

    Adding foundation

---
+++ client/src/views/Foundation.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header/Header";
+import FoundationATV from "../components/Foundation/FoundationATV";
+
+function Foundation() {
+  return (
+    <div>
+        <Header />
+        <FoundationATV />
+    </div>
+  )
+}
+
+export default Foundation

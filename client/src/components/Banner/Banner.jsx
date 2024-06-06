ad31f4a2dcee86c794f5ff8721920c58e685a457
brallam delgado <brallamdelgado351@gmail.com>
2024-06-05 20:21:31 -0500

    ending banner

--- client/src/components/Banner/Banner.jsx
+++ client/src/components/Banner/Banner.jsx
@@ -14,62 +14,76 @@ import "./Banner.css";
 
 const Banner = () => {
   
-  
-
   return (
     <div className="banner-container">
       <Carousel className='carousel'>
         <Carousel.Item>
+          <div className='container-banner'>
           <img className="d-block w-100 h-100" src={banner1} alt="Slide 1" />
+          </div>
           <div className="carousel-text">
             <h1>FOMENTAMOS LA PERMANENCIA ESTUDIANTIL</h1>
             <p>A TRAVÉS DEL APOYO SOCIOPEDAGÓGICO, LAS COMUNICACIONES Y LA ANALÍTICA DE DATOS.</p>
           </div>
         </Carousel.Item>
         <Carousel.Item>
+          <div className='container-banner'>
           <img className="d-block w-100 h-100" src={banner2} alt="Slide 2" />
+          </div>
           <div className="carousel-text">
             <h1>FOMENTAMOS LA PERMANENCIA ESTUDIANTIL</h1>
             <p>A TRAVÉS DEL APOYO SOCIOPEDAGÓGICO, LAS COMUNICACIONES Y LA ANALÍTICA DE DATOS</p>
           </div>
         </Carousel.Item>
         <Carousel.Item>
+          <div className='container-banner'>
           <img className="d-block w-100 h-100" src={banner3} alt="Slide 3" />
+          </div>
           <div className="carousel-text">
             <h1>FOMENTAMOS LA PERMANENCIA ESTUDIANTIL</h1>
             <p>A TRAVÉS DEL APOYO SOCIOPEDAGÓGICO, LAS COMUNICACIONES Y LA ANALÍTICA DE DATOS</p>
           </div>
         </Carousel.Item>
         <Carousel.Item>
+          <div className='container-banner'>
           <img className="d-block w-100 h-100" src={banner4} alt="Slide 4" />
+          </div>
           <div className="carousel-text">
             <h1>FOMENTAMOS LA PERMANENCIA ESTUDIANTIL</h1>
             <p>A TRAVÉS DEL APOYO SOCIOPEDAGÓGICO, LAS COMUNICACIONES Y LA ANALÍTICA DE DATOS</p>
           </div>
         </Carousel.Item>
         <Carousel.Item>
+          <div className='container-banner'>
           <img className="d-block w-100 h-100" src={banner5} alt="Slide 5" />
+          </div>
           <div className="carousel-text">
             <h1>FOMENTAMOS LA PERMANENCIA ESTUDIANTIL</h1>
             <p>A TRAVÉS DEL APOYO SOCIOPEDAGÓGICO, LAS COMUNICACIONES Y LA ANALÍTICA DE DATOS</p>
           </div>
         </Carousel.Item>
         <Carousel.Item>
+          <div className='container-banner'>
           <img className="d-block w-100 h-100" src={banner6} alt="Slide 6" />
+          </div>
           <div className="carousel-text">
             <h1>FOMENTAMOS LA PERMANENCIA ESTUDIANTIL</h1>
             <p>A TRAVÉS DEL APOYO SOCIOPEDAGÓGICO, LAS COMUNICACIONES Y LA ANALÍTICA DE DATOS</p>
           </div>
         </Carousel.Item>
         <Carousel.Item>
+          <div className='container-banner'>
           <img className="d-block w-100 h-100" src={banner7} alt="Slide 7" />
+          </div>
           <div className="carousel-text" >
             <h1>FOMENTAMOS LA PERMANENCIA ESTUDIANTIL</h1>
             <p>A TRAVÉS DEL APOYO SOCIOPEDAGÓGICO, LAS COMUNICACIONES Y LA ANALÍTICA DE DATOS</p>
           </div>
         </Carousel.Item>
         <Carousel.Item>
+          <div className='container-banner'>
           <img className="d-block w-100 h-100" src={banner8} alt="Slide 7" />
+          </div>
           <div className="carousel-text" >
             <h1>FOMENTAMOS LA PERMANENCIA ESTUDIANTIL</h1>
             <p>A TRAVÉS DEL APOYO SOCIOPEDAGÓGICO, LAS COMUNICACIONES Y LA ANALÍTICA DE DATOS</p>

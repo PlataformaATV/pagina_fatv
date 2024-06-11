b369a9fdf527890453b699c939f70b1f7039f6be
Monalisa330 <laurameliza26@gmail.com>
2024-06-10 23:14:01 -0500

    Uploading foundation team

--- client/src/components/Foundation/FoundationATV.jsx
+++ client/src/components/Foundation/FoundationATV.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
-import 'boxicons'
-import imgFB from '../../../public/img_Foundation/imgF.jpg'
-import imgF2 from '../../../public/img_Foundation/imgF2.jpg'
-import imgF3 from '../../../public/img_Foundation/imgF3.jpg'
+import 'boxicons';
+import imgFB from '../../../public/img_Foundation/imgF.jpg';
+import imgF2 from '../../../public/img_Foundation/imgF2.jpg';
+import imgF3 from '../../../public/img_Foundation/imgF3.jpg';
 import OurTeam from '../ourTeam/OurTeam';
 import Footer from '../Footer/Footer';
-import "./FoundationATV.css"
-
-
+import "./FoundationATV.css";
 
 function FoundationATV() {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,72 +17,178 @@ function FoundationATV() {
   const closePopup = () => {
     setIsOpen(false);
   };
-  
 
   return (
     <div className="relative w-full h-60 sm:h-80 md:h-96 "> 
       <img src={imgFB} alt="Banner" className="w-full h-full object-cover" />
       <div className="absolute inset-0 bg-blue-links bg-opacity-20"></div>
       <h1 className="absolute inset-0 flex items-center justify-center text-banner uppercase text-4xl sm:text-5xl md:text-7xl lg:text-8xl font-anton text-banner">
-      NUESTRA FUNDACIÓN
+        NUESTRA FUNDACIÓN
       </h1>
       <div className="absolute bottom-4 left-4 flex space-x-2 sm:space-x-4">
-      <a
-        href="https://www.facebook.com/people/Fundaci%C3%B3n-Antivirus-para-la-Deserci%C3%B3n/100089714876149/?mibextid=LQQJ4d"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="box-icon"
-      >
-        <box-icon name="facebook-circle" type="logo" color="#ffffff"></box-icon>
-      </a>
-      <a
+        <a
+          href="https://www.facebook.com/people/Fundaci%C3%B3n-Antivirus-para-la-Deserci%C3%B3n/100089714876149/?mibextid=LQQJ4d"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="box-icon"
+        >
+          <box-icon name="facebook-circle" type="logo" color="#ffffff"></box-icon>
+        </a>
+        <a
           href="https://www.instagram.com/somosantivirus/"
           target="_blank"
           rel="noopener noreferrer"
           className="box-icon"
         >
           <box-icon name="instagram-alt" type="logo" color="#ffffff"></box-icon>
-      </a>
-      <a
+        </a>
+        <a
           href="https://www.youtube.com/channel/UCCDsmMeIqSWGk_fh1m9FX0w"
           target="_blank"
           rel="noopener noreferrer"
           className="box-icon"
         >
           <box-icon name="youtube" type="logo" color="#ffffff"></box-icon>
-      </a>
-      <a
+        </a>
+        <a
           href="https://www.tiktok.com/@somosantivirus"
           target="_blank"
           rel="noopener noreferrer"
           className="box-icon"
         >
           <box-icon name="tiktok" type="logo" color="#ffffff"></box-icon>
-      </a>
-      <a
+        </a>
+        <a
           href="https://www.linkedin.com/company/antivirus-desercion/"
           target="_blank"
           rel="noopener noreferrer"
           className="box-icon"
         >
           <box-icon name="linkedin-square" type="logo" color="#ffffff"></box-icon>
-      </a>
+        </a>
       </div>
 
       <div className="aboutUs">
-      <div className="aboutUs-content">
-        <div className="left-content">
-          <p className="title">¿Quiénes Somos?</p>
-          <h4 className="subtitle">
-          Somos una organización sin ánimo de lucro que
-          </h4>
-        </div>
-        <div className="right-content">
-          <p className="right-paragraph">
-          Busca disminuir los niveles de deserción estudiantil en Colombia, mejorar los niveles de permanencia y de graduación, siendo esta nuestra manera de aportar al logro de un país con más educación y consecuentemente con más desarrollo.
-          </p>
+        <div className="aboutUs-content">
+          <div className="left-content">
+            <p className="title">¿Quiénes Somos?</p>
+            <h4 className="subtitle">
+              Somos una organización sin ánimo de lucro que
+            </h4>
+          </div>
+          <div className="right-content">
+            <p className="right-paragraph">
+              Busca disminuir los niveles de deserción estudiantil en Colombia, mejorar los niveles de permanencia y de graduación, siendo esta nuestra manera de aportar al logro de un país con más educación y consecuentemente con más desarrollo.
+            </p>
+          </div>
         </div>
       </div>
+
+      <div className='foundation_team'>
+        <div className='foundation_team--cnt'>
+          <div className='foundation_team--content'>
+            <div className='foundation_team--block'>
+              <div className='_title'>
+                <p>
+                  <small>NUESTROS</small>
+                </p>
+                <h5>FUNDADORES</h5>
+              </div>
+              <div className='_members'>
+                <p>
+                  Carlos
+                  <strong>Vásquez Restrepo</strong>
+                </p>
+                <p>
+                  Paulina
+                  <strong>Tamayo Mejía</strong>
+                </p>
+                <p>
+                  Víctor Manuel
+                  <strong>Valencia Martínez</strong>
+                </p>
+                <p>
+                  Luis Fernando
+                  <strong>González Urán</strong>
+                </p>
+                <p>
+                  Andrea
+                  <strong>Jaramillo Ramírez</strong>
+                </p>
+                <p>
+                  Laura
+                  <strong>Calle Escobar</strong>
+                </p>
+                <p>
+                  León Felipe
+                  <strong>Hernández López</strong>
+                </p>
+                <p>
+                  Aida
+                  <strong>Orduz Quijano</strong>
+                </p>
+              </div>
+            </div>
+            <div className='foundation_team--block'>
+              <div className='_title'>
+                <p>
+                  <small>NUESTRA</small>
+                </p>
+                <h5>JUNTA DIRECTIVA</h5>
+              </div>
+              <div className='_members'>
+                <p>
+                  Carlos Vásquez Restrepo
+                  <strong>Presidente Y Representante Legal Titular</strong>
+                </p>
+                <p>
+                  Luis Fernando Sánchez Hurtado
+                  <strong>Vicepresidente / Director y Primer Representante Legal Suplente</strong>
+                </p>
+                <p>
+                  David Santiago Botero Rodríguez
+                  <strong>Segundo Representante Legal Suplente / Coordinador Legal</strong>
+                </p>
+                <p>
+                  Víctor Manuel Valencia Martínez
+                  <strong>Subdirector / Tercer Representante Legal Suplente</strong>
+                </p>
+                <p>
+                  Luis Fernando
+                  <strong>González Urán</strong>
+                </p>
+              </div>
+            </div>
+            <div className='foundation_team--block'>
+              <div className='_title'>
+                <p>
+                  <small>NUESTRO</small>
+                </p>
+                <h5>REVISOR FISCAL</h5>
+              </div>
+              <div className='_members'>
+                <p>
+                  Orlando
+                  <strong>Gaviria Flórez</strong>
+                </p>
+              </div>
+            </div>
+            <div className='foundation_team--block'>
+              <div className='_title'>
+                <p>
+                  <small>NUESTRA</small>
+                </p>
+                <h5>CONTADORA</h5>
+              </div>
+              <div className='_members'>
+                <p>
+                  María Verónica
+                  <strong>Ortiz Rodríguez</strong>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className='flex justify-end'>

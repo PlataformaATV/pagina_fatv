a950f91d7ddd4c5643e83bd80d2af76f6d8977c3
Deiiivy <deivypr28@gmail.com>
2024-06-27 19:03:11 -0500

    add structure

--- client/src/components/Projects_ATV/Projects.jsx
+++ client/src/components/Projects_ATV/Projects.jsx
@@ -2,10 +2,68 @@ import React from 'react'
 import './Projects.css'
 import Footer from '../Footer/Footer';
 import imgBannerATV from '../../../public/img_DataAnalytics/banner-DataAnalytics.jpg'
+import { useState, useEffect } from 'react';
 
 
 function Projects() {
+
+  const [socialMedia, setSocialMedia] = useState([
+    {
+      href: "https://www.facebook.com/people/Fundaci%C3%B3n-Antivirus-para-la-Deserci%C3%B3n/100089714876149/?mibextid=LQQJ4d",
+      name: "facebook-circle",
+      type: "logo",
+      color: "#ffffff"
+    },
+    {
+      href: "https://www.instagram.com/somosantivirus/",
+      name: "instagram-alt",
+      type: "logo",
+      color: "#ffffff"
+    },
+    {
+      href: "https://www.youtube.com/channel/UCCDsmMeIqSWGk_fh1m9FX0w",
+      name: "youtube",
+      type: "logo",
+      color: "#ffffff"
+    },
+    {
+      href: "https://www.tiktok.com/@somosantivirus",
+      name: "tiktok",
+      type: "logo",
+      color: "#ffffff"
+    },
+    {
+      href: "https://www.linkedin.com/company/antivirus-desercion/",
+      name: "linkedin-square",
+      type: "logo",
+      color: "#ffffff"
+    }
+  ])
+
+  const[proyectsContent, setProyectsContent] =useState([
+    {
+      title: "Universidades",
+      img: "https://estaticos.elcolombiano.com/binrepository/580x365/0c0/0d0/none/11101/CFOY/udea_38598473_20210923105540.jpg",
+      name: "universidades"
+    },
+    {
+      title: "BootCamps",
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4sSUWjF6jkDN3m9Et_JQTunBm1dYY0NoKeA&s",
+      name: "bootcamps"
+    },
+    {
+      title: "Colegios",
+      img: "https://bogota.gov.co/sites/default/files/styles/1050px/public/2023-05/laureldeceraokpg.jpeg",
+      name: "colegios"
+    },
+    {
+      title: "Becarios",
+      img: "https://empresas.infoempleo.com/hrtrends/media/2023/02/Empresa-que-decide-contratar-a-becarios.jpg",
+      name: "Becarios"
+    }
+  ])
     return (
+      <>
       <div>
         <div className='flex flex-col items-center justify-center'>
           <div className="relative w-full h-60 sm:h-80 md:h-96">
@@ -15,53 +73,44 @@ function Projects() {
               PROYECTOS
             </h1>
             <div className="absolute bottom-4 left-4 flex space-x-2 sm:space-x-4">
-              <a
-                href="https://www.facebook.com/people/Fundaci%C3%B3n-Antivirus-para-la-Deserci%C3%B3n/100089714876149/?mibextid=LQQJ4d"
+
+              {socialMedia.map((social,index) => (
+                <a
+                key={index}
+                href={social.href}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="box-icon"
               >
-                <box-icon name="facebook-circle" type="logo" color="#ffffff"></box-icon>
+                <box-icon name={social.name} type={social.type} color={social.color}></box-icon>
               </a>
-              <a
-                href="https://www.instagram.com/somosantivirus/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="box-icon"
-              >
-                <box-icon name="instagram-alt" type="logo" color="#ffffff"></box-icon>
-              </a>
-              <a
-                href="https://www.youtube.com/channel/UCCDsmMeIqSWGk_fh1m9FX0w"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="box-icon"
-              >
-                <box-icon name="youtube" type="logo" color="#ffffff"></box-icon>
-              </a>
-              <a
-                href="https://www.tiktok.com/@somosantivirus"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="box-icon"
-              >
-                <box-icon name="tiktok" type="logo" color="#ffffff"></box-icon>
-              </a>
-              <a
-                href="https://www.linkedin.com/company/antivirus-desercion/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="box-icon"
-              >
-                <box-icon name="linkedin-square" type="logo" color="#ffffff"></box-icon>
-              </a>
+              ))}
+
             </div>
+          </div>  
+        </div>
+        </div>
+
+        <div className='flex flex-col items-center justify-center'>
+
+        <div className='flex flex-col justify-center text-center'>
+              <h1 className='text-3xl'>Nuestros Proyectos</h1>
           </div>
 
-          
-        </div>
+
+
+          <div className='projects-content'>
+          {proyectsContent.map((project, index) => (
+            <div key={index} >
+              <h1 className='text-2xl'>{project.title}</h1>
+              <img className='"object-cover h-48 w-96 rounded-3xl' src={project.img} alt={project.name} />
+            </div>
+          ))}
+          </div>
+
+          </div>
         <Footer />
-      </div>
+        </>
     );
   }
   export default Projects;

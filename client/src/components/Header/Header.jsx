0d838e8c0c02c57cce2d90fed884d9da59a09eae
deiiivy <deivypr28@gmail.com>
2024-07-08 18:09:57 -0500

    add fonts

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -106,42 +106,41 @@ function Header() {
 
   return (
     <div>
-      <header onMouseLeave={handleMouseLeave} className={`hidden z-50 flex lg:flex justify-between items-center py-3 px-16 max-md:py-2 bg-white fixed w-full transition-all duration-300 ${isHidden ? '-translate-y-full' : 'translate-y-0'}`}>
-        <div className='Links-left flex gap-1 items-center'>
-          <Link to="/" onClick={handleNav} className='text-title font-extrabold uppercase transition duration-400 ease-in-out max-xl:text-sm object-contain' style={{ letterSpacing: '-1px' }}>Inicio</Link>
-          <span className='text-blue-links mx-1'>|</span>
-          <Link to="/Fundacion" className='text-title font-extrabold uppercase leading-none transition duration-400 ease-in-out max-xl:text-sm' style={{ letterSpacing: '-1px' }}>FUNDACIÓN</Link>
-          <span className='text-title mx-1'>|</span>
-          <button onClick={handleClikPopupQH} className='flex items-center text-title font-extrabold uppercase leading-none transition duration-400 ease-in-out max-xl:text-sm' style={{ letterSpacing: '-1px' }}>QUÉ HACEMOS</button>
-          <span className='text-title mx-1'>|</span>
-          <button onClick={handleClikPopupServices} className='text-title font-extrabold uppercase leading-none transition duration-400 ease-in-out max-xl:text-sm' style={{ letterSpacing: '-1px' }}>SERVICIOS</button>
-        </div>
-
-        <div className='logo'>
-          <img className="h-28 object-contain max-xl:h-24 max-lg:h-14" src="/logo.png" alt="logo" />
-        </div>
-
-        <div className='Links-right flex gap-3 items-center'>
-          <Link to="/Proyectos" className='text-title font-extrabold uppercase leading-none transition duration-400 ease-in-out max-xl:text-sm' style={{ letterSpacing: '-1px' }}>Proyectos</Link>
-          <span className='text-title mx-1'>|</span>
-          <Link to="/ContactUs" className='text-title font-extrabold uppercase leading-none transition duration-400 ease-in-out max-xl:text-sm' style={{ letterSpacing: '-1px' }}>CONTÁCTENOS</Link>
-          <span className='text-title mx-1'>|</span>
-          <button onClick={handleClikPopupIdioma} className='flex items-center justify-center text-title font-extrabold uppercase leading-none transition duration-400 ease-in-out' style={{ letterSpacing: '-1px' }}>
-            ESP
-            <svg className='ml-1 text-blue-300 w-3 h-3' viewBox="0 0 5 8" xmlns="http://www.w3.org/2000/svg">
-              <path d="M0.998481 8C0.668876 7.67019 0.340995 7.3421 0.0182923 7.01917C1.01574 6.02528 2.02285 5.0221 3.0303 4.01857C1.998 2.99304 0.990198 1.99192 0 1.008C0.357217 0.651707 0.688548 0.321898 1.01125 0C2.32484 1.31648 3.66846 2.66288 5 3.99759C3.68503 5.3127 2.34348 6.65463 0.998481 8Z"></path>
-            </svg>
-          </button>
-
-          <button className='flex items-center justify-center py-3 px-4 bg-btn-back rounded-3xl text-white font-bold hover:bg-orange-500 transition duration-700 transform hover:scale-105 max-xl:py-2 px-3'>
-            <div className="flex items-center">
-              Apóyanos
-              <box-icon name='gift' color='#eae9e9'></box-icon>
+       <header onMouseLeave={handleMouseLeave} className={`hidden z-50 flex lg:flex justify-between items-center py-3 px-16 max-md:py-2 bg-white fixed w-full transition-all duration-300 ${isHidden ? '-translate-y-full' : 'translate-y-0'}`}>
+            <div className='Links-left flex gap-1 items-center'>
+                <Link to="/" onClick={handleNav} className='text-lg text-title font-extrabold uppercase transition duration-400 ease-in-out max-xl:text-sm object-contain' style={{ letterSpacing: '-1px', fontFamily: 'Acumin2' }}>Inicio</Link>
+                <span className='text-blue-links mx-1'>|</span>
+                <Link to="/Fundacion" className='text-lg text-title font-extrabold uppercase leading-none transition duration-400 ease-in-out max-xl:text-sm' style={{ letterSpacing: '-1px', fontFamily: 'Acumin2' }}>FUNDACIÓN</Link>
+                <span className='text-title mx-1'>|</span>
+                <button onClick={handleClikPopupQH} className='flex items-center text-lg text-title font-extrabold uppercase leading-none transition duration-400 ease-in-out max-xl:text-sm' style={{ letterSpacing: '-1px', fontFamily: 'Acumin2' }}>QUÉ HACEMOS</button>
+                <span className='text-title mx-1'>|</span>
+                <button onClick={handleClikPopupServices} className='text-lg text-title font-extrabold uppercase leading-none transition duration-400 ease-in-out max-xl:text-sm' style={{ letterSpacing: '-1px', fontFamily: 'Acumin2' }}>SERVICIOS</button>
             </div>
-          </button>
-        </div>
-      </header>
 
+            <div className='logo'>
+                <img className="h-28 object-contain max-xl:h-24 max-lg:h-14" src="/logo.png" alt="logo" />
+            </div>
+
+            <div className='Links-right flex gap-3 items-center'>
+                <Link to="/Proyectos" className='text-lg text-title font-extrabold uppercase leading-none transition duration-400 ease-in-out max-xl:text-sm' style={{ letterSpacing: '-1px', fontFamily: 'Acumin2' }}>Proyectos</Link>
+                <span className='text-title mx-1'>|</span>
+                <Link to="/ContactUs" className='text-lg text-title font-extrabold uppercase leading-none transition duration-400 ease-in-out max-xl:text-sm' style={{ letterSpacing: '-1px', fontFamily: 'Acumin2' }}>CONTÁCTENOS</Link>
+                <span className='text-title mx-1'>|</span>
+                <button onClick={handleClikPopupIdioma} className='flex items-center justify-center text-lg text-title font-extrabold uppercase leading-none transition duration-400 ease-in-out' style={{ letterSpacing: '-1px', fontFamily: 'Acumin2' }}>
+                    ESP
+                    <svg className='ml-1 text-blue-300 w-3 h-3' viewBox="0 0 5 8" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0.998481 8C0.668876 7.67019 0.340995 7.3421 0.0182923 7.01917C1.01574 6.02528 2.02285 5.0221 3.0303 4.01857C1.998 2.99304 0.990198 1.99192 0 1.008C0.357217 0.651707 0.688548 0.321898 1.01125 0C2.32484 1.31648 3.66846 2.66288 5 3.99759C3.68503 5.3127 2.34348 6.65463 0.998481 8Z"></path>
+                    </svg>
+                </button>
+
+                <button className='flex items-center justify-center py-3 px-4 bg-btn-back rounded-3xl text-white font-bold hover:bg-orange-500 transition duration-700 transform hover:scale-105 max-xl:py-2 px-3' style={{ fontFamily: 'Acumin2' }}>
+                    <div className="flex items-center">
+                        Apóyanos
+                        <box-icon name='gift' color='#eae9e9'></box-icon>
+                    </div>
+                </button>
+            </div>
+        </header>
       <div className={`${nav ? 'hidden' : 'flex justify-between bg-white items-center px-6 py-2'}`}>
         <div className='logo-responsive-menu'>
           <img className="h-28 object-contain max-sm:h-16" src="/logo.png" alt="logo" />

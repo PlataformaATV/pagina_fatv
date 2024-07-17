6572d86f2491b1df5ec7f7c2153ecde0b45639aa
samuelzapatam22 <samuelzapatam1@gmail.com>
2024-07-17 15:28:40 -0500

    resolving mistakes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import ContacUs from './views/ContacUs.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ContacUs/>
+    <App/>
   </React.StrictMode>,
 )

722c49f08f75a50a7b0d40589cbd838869c9e405
Lina Marcela Marin <linamarin2011@gmail.com>
2025-03-09 18:34:49 -0500

    add style not found

--- client/src/views/NotFound.jsx
+++ client/src/views/NotFound.jsx
@@ -1,10 +1,30 @@
-import './NotFound.css';
+import "./NotFound.css";
+import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <section className="grid justify-center items-center h-screen">
-      <h1 id="errorNumber" className="text-9xl font-bold ">404</h1>
-      <p class="mt-1 text-4xl font-medium  text-gray-500"><br />Sorry! Página no encontrada</p>
+    <section
+      id="not-found"
+      className="grid justify-center items-center h-screen bg-cover text-center"
+    >
+      <div>
+        <h1 id="errorNumber" className="font-bold block m-auto">
+          404
+        </h1>
+        <p className="text-4xl font-semibold text-center">Oops!</p>
+        <p className="mt-1 text-2xl text-gray-500 font-medium text-center">
+          <br />
+          Lo sentimos, página no encontrada
+        </p>
+        <Link to="/">
+          <button
+            id="volver-inicio"
+            className="text-md mt-3 p-3 rounded-3xl border-2"
+          >
+            Volver al Inicio
+          </button>
+        </Link>
+      </div>
     </section>
   );
 }
